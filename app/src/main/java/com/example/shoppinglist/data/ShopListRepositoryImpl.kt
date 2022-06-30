57524052a6704526aa0557bd96366c2c90b2dbd1
Serg <dolgorukovs11629@gmail.com>
2022-06-30 18:16:39 +0300

    Created ShopListRepositoryImpl for future realisation of methods and changed access modifiers

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl:ShopListRepository {
+
+    override fun addShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
 data class ShopItem(
-    private val id:Int,
-    private val count:Int,
-    private val name:String,
-    private val enabled:Boolean)
+     val id:Int,
+     val count:Int,
+     val name:String,
+     val enabled:Boolean)
